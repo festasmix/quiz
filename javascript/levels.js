94a1c55b2854ffeb83e1a83474b406db4349bbda
festasmix <webmaster@festasmix.com.br>
2016-02-04 14:57:30 -0200

    Update levels.js

--- javascript/levels.js
+++ javascript/levels.js
@@ -4,26 +4,26 @@ app.factory("levels", function() {
   var levels = [
     {
       minimun_score: 0,
-  		title: "Calouro do Marketing Digital",
-  		text: "Você começou a trilhar agora o seu caminho em marketing digital. Continue firme e forte!",
+  		title: "Mãe de primeira Viajem",
+  		text: "Você consegue cuidar do seu filho, mas ainda tem muito que melhorar. Continue firme e forte!",
       image: "images/freshman.png"
   	},
     {
-      minimun_score: 11,
-  		title: "Recém-graduado em Marketing Digital",
-  		text: "Você já aprendeu o essencial e está pronto para seguir seu caminho!",
+      minimun_score: 1,
+  		title: "Mãezinha",
+  		text: "Seu filho já deve ter mas de 6 meses, parabéns. mas você ainda tem muito que aprender com ele.",
       image: "images/graduated.png"
   	},
     {
-      minimun_score: 16,
-  		title: "Expert em Marketing Digital",
-  		text: "Você já sabe quase tudo de marketing digital.",
+      minimun_score: 2,
+  		title: "Mãe de verdade",
+  		text: "Você já sabe quase tudo sobre seu filho.",
       image: "images/expert.png"
   	},
     {
       minimun_score: 20,
-      title: "Ninja do Marketing Digital",
-      text: "Você sabe tudo de marketing digital. Me dá um autógrafo?",
+      title: "Vovó, ;)",
+      text: "Você deve ter uns 2 filhos e 3 netos, impressionante sabe de tudo.",
       image: "images/ninja.png"
     }
   ];
